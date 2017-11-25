7c7d1fb7d7e904fd0d5d3f8271dabafdb275dd46
Denis <denisdenisdenis@bigmir.net>
2017-11-25 20:54:43 +0200

    add CRUD

--- src/main/scala/model/Task.scala
+++ src/main/scala/model/Task.scala
@@ -2,6 +2,8 @@ package model
 
 import slick.jdbc.PostgresProfile.api._
 
+import scala.concurrent.Future
+
 case class Task(id: Long, title: String, isDone: Boolean, createdAt: Long, userId: Long)
 
 class TaskTable(tag: Tag) extends Table[Task](tag, "tasks") {
@@ -20,4 +22,18 @@ object TaskTable {
   val tasks = TableQuery[TaskTable]
 }
 
+class TaskRepository(db: Database) {
+  val tasksTableQuery = TableQuery[TaskTable]
 
+  def create(task: Task): Future[Task] =
+    db.run(tasksTableQuery returning tasksTableQuery += task)
+
+  def update(task: Task): Future[Int] =
+    db.run(tasksTableQuery.filter(_.id === task.id).update(task))
+
+  def delete(taskId: Long): Future[Int] =
+    db.run(tasksTableQuery.filter(_.id === taskId).delete)
+
+  def getById(taskId: Long): Future[Option[Task]] =
+    db.run(tasksTableQuery.filter(_.id === taskId).result.headOption)
+}
--- src/main/scala/model/User.scala
+++ src/main/scala/model/User.scala
@@ -2,6 +2,8 @@ package model
 
 import slick.jdbc.PostgresProfile.api._
 
+import scala.concurrent.Future
+
 case class User(id: Long, name: String, password: String)
 
 class UserTable(tag: Tag) extends Table[User](tag, "users") {
@@ -14,4 +16,22 @@ class UserTable(tag: Tag) extends Table[User](tag, "users") {
 
 object UserTable {
   val users = TableQuery[UserTable]
+}
+
+class UserRepository(db: Database) {
+  val usersTableQuery = TableQuery[UserTable]
+
+  def create(user: User): Future[User] =
+    db.run(usersTableQuery returning usersTableQuery += user)
+
+  def update(user: User): Future[Int] =
+    db.run(usersTableQuery.filter(_.id === user.id).update(user))
+
+  def delete(userId: Long): Future[Int] =
+    db.run(usersTableQuery.filter(_.id === userId).delete)
+
+  def getUserById(userId: Long): Future[Option[User]] =
+    db.run(usersTableQuery.filter(_.id === userId).result.headOption)
+
+
 }
